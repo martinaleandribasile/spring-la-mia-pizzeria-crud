b427811d8035b337244bf2809bbfc2ae13a353a9
MartinaBasile <martinaleandribasile@gmail.com>
2023-02-15 15:40:27 +0100

    dettails and research

--- src/main/java/com/java/pizzeria/controller/PizzaController.java
+++ src/main/java/com/java/pizzeria/controller/PizzaController.java
@@ -1,12 +1,15 @@
 package com.java.pizzeria.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.java.pizzeria.model.Pizza;
 import com.java.pizzeria.repository.PizzaRepository;
@@ -20,10 +23,26 @@ public class PizzaController {
 	PizzaRepository pizzaRepository;
 	
 	@GetMapping
-	public String index(Model model) {
-		List<Pizza> elencoPizze= pizzaRepository.findAll();
+	public String index(@RequestParam(name="keyword", required= false) String keyword,  Model model) {
+		//List<Pizza> elencoPizze= pizzaRepository.findByNomeLike("%ghe%"); ricerca in base alla stringa che inserisco come parametro di ricerca
+		List<Pizza> elencoPizze;
+		if(keyword!=null && !keyword.isEmpty()) {
+			elencoPizze= pizzaRepository.findByNomeLike("%"+ keyword +"%");
+		}else {
+			elencoPizze= pizzaRepository.findAll();
+		}
 		model.addAttribute("elencoPizze" , elencoPizze);
+		keyword="";
 		return "indexPizze";
-		
+	}
+	
+	@GetMapping("{id}") //richieste get /pizze/{id}
+	public String dettailsPizza(@PathVariable("id") Integer id, Model model) {
+		Optional<Pizza> p= pizzaRepository.findById(id);
+		if(p.isEmpty()) {
+			return "redirect:/error";
+		}
+		model.addAttribute("pizza",p.get());
+		return "dettaglioPizza";
 	}
 }
